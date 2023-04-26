5b20762303af53da0bb85e2333cc93a281f0d496
Ahri Kitsune <meowsliru@gmail.com>
2023-04-26 17:12:46 +0700

    Add project files.

---
+++ Scrum Learn/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scrum_Learn.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+
+    }
+}
